0147f52865ebe6f753754a8f761ce896f58c6a84
Badfic Santiago <badfic@outlook.com>
2024-07-12 00:41:25 -0700

    update to latest dependencies

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import { Router } from "itty-router";
+import { AutoRouter  } from "itty-router";
 import {
   InteractionResponseType,
   InteractionType,
@@ -7,25 +7,28 @@ import {
 import { AO3_COMMAND, INVITE_COMMAND } from "./commands.js";
 import { InteractionResponseFlags } from "discord-interactions";
 
-class JsonResponse extends Response {
-  constructor(body, init) {
-    const jsonBody = JSON.stringify(body);
-    init = init || {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-      },
-    };
-    super(jsonBody, init);
+async function verifyDiscordRequest(request, env) {
+  const signature = request.headers.get("x-signature-ed25519");
+  const timestamp = request.headers.get("x-signature-timestamp");
+  const body = await request.text();
+  const isValidRequest =
+      signature &&
+      timestamp &&
+      await verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
+  if (!isValidRequest) {
+    return { isValid: false };
   }
+
+  return { interaction: JSON.parse(body), isValid: true };
 }
 
-const router = Router();
+const router = AutoRouter();
 
 /**
  * A simple :wave: hello page to verify the worker is working.
  */
 router.get("/", (request, env) => {
-  return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+  return `👋 ${env.DISCORD_APPLICATION_ID}`;
 });
 
 /**
@@ -34,10 +37,7 @@ router.get("/", (request, env) => {
  * https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
  */
 router.post("/", async (request, env) => {
-  const { isValid, interaction } = await server.verifyDiscordRequest(
-    request,
-    env,
-  );
+  const { isValid, interaction } = await verifyDiscordRequest(request, env);
   if (!isValid || !interaction) {
     return new Response("Bad request signature.", { status: 401 });
   }
@@ -45,9 +45,9 @@ router.post("/", async (request, env) => {
   if (interaction.type === InteractionType.PING) {
     // The `PING` message is used during the initial webhook handshake, and is
     // required to configure the webhook in the developer portal.
-    return new JsonResponse({
+    return {
       type: InteractionResponseType.PONG,
-    });
+    };
   }
 
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
@@ -63,12 +63,12 @@ router.post("/", async (request, env) => {
 
           if (!ao3Url.match(ao3UrlRegex)) {
             console.log(`URL was not an ao3 url: ${ao3Url}`);
-            return new JsonResponse({
+            return {
               type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
               data: {
                 content: "Unrecognized AO3 url"
               }
-            });
+            };
           }
 
           const apiRequest = JSON.stringify({
@@ -88,68 +88,45 @@ router.post("/", async (request, env) => {
           console.log(`Received status=${fetchResponse.status} from AO3 Summary API`);
 
           if (fetchResponse.status === 400) {
-            return new JsonResponse({
+            return {
               type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
               data: {
                 content: "Unrecognized Request"
               }
-            });
+            };
           } else if (fetchResponse.status === 200) {
-            return new JsonResponse({
+            return {
               type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE
-            });
+            };
           }
         } catch (e) {
           console.error(e);
         }
 
-        return new JsonResponse({
+        return {
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
             content: "Please try again later"
           }
-        });
+        };
       }
       case INVITE_COMMAND.name.toLowerCase(): {
         const applicationId = env.DISCORD_APPLICATION_ID;
         const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${applicationId}&scope=applications.commands`;
-        return new JsonResponse({
+        return {
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
             content: INVITE_URL,
             flags: InteractionResponseFlags.EPHEMERAL,
           }
-        });
+        };
       }
       default:
-        return new JsonResponse({ error: "Unknown Type" }, { status: 400 });
+        return new Response({ error: "Unknown Type" }, { status: 400 });
     }
   }
 
-  return new JsonResponse({ error: "Unknown Type" }, { status: 400 });
+  return new Response({ error: "Unknown Type" }, { status: 400 });
 });
-router.all("*", () => new Response("Not Found.", { status: 404 }));
 
-async function verifyDiscordRequest(request, env) {
-  const signature = request.headers.get("x-signature-ed25519");
-  const timestamp = request.headers.get("x-signature-timestamp");
-  const body = await request.text();
-  const isValidRequest =
-    signature &&
-    timestamp &&
-    verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
-  if (!isValidRequest) {
-    return { isValid: false };
-  }
-
-  return { interaction: JSON.parse(body), isValid: true };
-}
-
-const server = {
-  verifyDiscordRequest: verifyDiscordRequest,
-  fetch: async function (request, env) {
-    return router.handle(request, env);
-  },
-};
-
-export default server;
+export default router;
